4d25c91bd887b30d9348293b65d0cd56a0b3b2f1
leny <info@flatland.be>
2016-10-09 00:05:03 +0200

    Base for scripts

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/* leny/flatland.be
+ *
+ * /src/js/main.js - Main entry point
+ *
+ * coded by leny@flatLand!
+ * started at 08/10/2016
+ */
+
+import $ from "jquery";
+
+$( () => {
+
+    console.log( "flatLand! 2016" );
+
+} );
